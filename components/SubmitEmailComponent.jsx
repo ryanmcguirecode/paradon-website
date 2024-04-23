7f2b9ce41a5639b62eed55180b43681349cc0b8b
Ryan McGuire <ryanmcguire@berkeley.edu>
2024-04-23 17:04:21 -0400

    move send message to backend

--- components/SubmitEmailComponent.jsx
+++ components/SubmitEmailComponent.jsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { Box, Input, Button } from "@mui/joy";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import { NextResponse, NextRequest } from "next/server";
 
 export default function SubmitEmailComponent() {
   const [email, setEmail] = useState("");
@@ -24,37 +23,13 @@ export default function SubmitEmailComponent() {
 
   const handleSubmit = async function () {
     setLoading(true);
-    const sendMessage = async function () {
-      const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
-      const chatId = process.env.CHAT_ID;
-      const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
-
-      const body = await req.json();
-      const message = body.message;
-
-      const params = {
-        chat_id: chatId,
-        text: message,
-      };
-
-      try {
-        const response = await fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(params),
-        });
-        const data = await response.json();
-        console.log("Message sent:", data);
-        return NextResponse.json(data);
-      } catch (error) {
-        console.error("Error sending message:", error);
-        return NextResponse.error("Error sending message.");
-      }
-    };
-
-    await sendMessage()
+    await fetch("/api/send-message", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: "New Lead: " + email }),
+    })
       .then((res) => res.json())
       .then((data) => console.log("Message sent:", data))
       .then(() => setSubmitted(true))
